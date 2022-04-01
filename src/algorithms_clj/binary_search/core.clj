2db7b409fb901cf42e24c41f6bbde847a1a35c74
Rende11 <artamonovn@gmail.com>
2022-04-01 10:30:53 +0300

    Add binary search implementation.

--- src/algorithms_clj/binary_search/core.clj
+++ src/algorithms_clj/binary_search/core.clj
@@ -1,5 +1,21 @@
 (ns algorithms-clj.binary-search.core)
 
-
 (defn search [coll target]
-  )
+  (loop [left 0
+         right (dec (count coll))]
+    (if (> left right)
+      -1
+      (let [mid (-> (- right left)
+                    (/ 2)
+                    (+ left)
+                    (int))
+            guess (nth coll mid)]
+        (cond
+          (= guess target)
+          mid
+
+          (> guess target)
+          (recur left (dec mid))
+
+          (< guess target)
+          (recur (inc mid) right))))))
--- test/algorithms_clj/binary_search/core_test.clj
+++ test/algorithms_clj/binary_search/core_test.clj
@@ -1,6 +1,15 @@
 (ns algorithms-clj.binary-search.core-test
   (:require [algorithms-clj.binary-search.core :as sut]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest testing is]]))
 
 (deftest test-binary-search
-  (is (= 3 (sut/search [1 2 3 4 5] 4))))
+  (testing "Target present in vector"
+    (is (= 0 (sut/search [100] 100)))
+    (is (= 3 (sut/search [-1 2 6 10 15] 10)))
+    (is (= 0 (sut/search [-1 2 6 10 15] -1)))
+    (is (= 100 (sut/search (vec (range 1000)) 100))))
+
+  (testing "Target is absent"
+    (is (= -1 (sut/search [-1 2 6 10 15] 100)))
+    (is (= -1 (sut/search [-1 2 6 10 15] -100)))
+    (is (= -1 (sut/search [] 100500)))))
