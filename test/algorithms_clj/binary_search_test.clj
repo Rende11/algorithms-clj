503409dd6190658aa9a5641dc2da1e0b078f6f46
Rende11 <artamonovn@gmail.com>
2022-04-01 10:35:50 +0300

    Delete old test file.

--- test/algorithms_clj/binary_search_test.clj
+++
@@ -1,7 +0,0 @@
-(ns algorithms-clj.binary-search-test
-  (:require [algorithms-clj.binary-search-test :as sut]
-            [clojure.test :refer [deftest testing is]]))
-
-(deftest binary-search
-  (is (= (sut/search [1 2 3 4 5])))
-  )
